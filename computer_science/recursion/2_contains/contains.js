f0630d9cb904066883b893c12a00071984828e04
BarreraClaudia <claudiabarrera.code@gmail.com>
2026-03-25 18:22:50 -0600

    complete recursion exercises

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,19 @@
-const contains = function() {
-  
+const contains = function (obj, targetValue) {
+  for (const key in obj) {
+    const val = obj[key];
+
+    const match = Number.isNaN(targetValue)
+      ? Number.isNaN(val)
+      : val === targetValue;
+
+    if (match) return true;
+
+    if (typeof val === 'object' && val !== null) {
+      if (contains(obj[key], targetValue)) return true;
+    }
+  }
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,22 @@
-const totalIntegers = function() {
-  
+const isObject = (obj) => typeof obj === 'object' && obj !== null;
+
+const totalIntegers = function (obj) {
+  if (!isObject(obj)) return;
+
+  let integers = 0;
+
+  let objValues = Object.values(obj);
+
+  for (const val of objValues) {
+    if (Number.isInteger(val)) {
+      integers++;
+    } else if (isObject(val)) {
+      integers += totalIntegers(val);
+    }
+  }
+
+  return integers;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
